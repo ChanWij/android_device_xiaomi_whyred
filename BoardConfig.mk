1b586e23406715fd29ccaedcce1250591fadd9d3
Davide Garberi <dade.garberi@gmail.com>
2019-04-24 13:12:42 +0200

    wayne: Add board extra root folders
    Change-Id: Id9b25142823f1732efd0cc447a62b8b1ff20c8c9
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -248,6 +248,7 @@ TARGET_USES_MKE2FS := true
 # System as root
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 BOARD_KERNEL_CMDLINE += skip_initramfs rootwait ro init=/init root=/dev/dm-0 dm=\"system none ro,0 1 android-verity /dev/mmcblk0p61\"
+BOARD_ROOT_EXTRA_FOLDERS := dsp firmware bt_firmware
 
 # Vendor init
 TARGET_INIT_VENDOR_LIB := libinit_whyred
